acfcfda613798d44393cce7c8efd7241f8b32e3d
Mantas <bemze007@yahoo.com>
2020-12-30 18:12:49 +0200

    done

--- js/main.js
+++ js/main.js
@@ -26,9 +26,6 @@ function sortData(list) {
     //     }
     // }
 
-console.log(sortedList);
-   
-    
 return sortedList;
 }
 
@@ -39,6 +36,16 @@ function lentelesGeneravimas(menesiuPavadinimai, pinigine) {
     income = 0;
     expense = 0;
 
+    /*jei nori zinoti, kiek daugiausiai uzdirbai, reikia su kazkuo lyginti didesniu, siuo atveju su infinity*/
+    minIncome = 0;
+    minIncomeValue = Infinity;
+    maxIncome = 0;
+    maxIncomeValue = -Infinity;
+    minExpense = 0;
+    minExpenseValue = Infinity;
+    maxExpense = 0;
+    maxExpenseValue = -Infinity;
+
     pinigine = sortData(pinigine);   /*piniginis balansas issikviecia jau isrusiuota savo paties balansa eiles tvarka*/
 
     for(let i = 0; i < pinigine.length; i++) {
@@ -46,6 +53,29 @@ function lentelesGeneravimas(menesiuPavadinimai, pinigine) {
         income += item.income ? item.income : 0;
         expense += item.expense ? item.expense : 0;
         balance = income - expense;
+
+        //uzdirbau daugiausia
+        if (item.income && item.income < minIncomeValue ) {
+            minIncomeValue = item.income;
+            minIncome = i;
+        }
+        //uzdirbau maziausia
+        if (item.income && item.income > maxIncomeValue ) {
+            maxIncomeValue = item.income;
+            maxIncome = i;
+        }
+        //isleidau daugiausia
+        if (item.expense && item.expense < minExpenseValue ) {
+            minExpenseValue = item.expense;
+            minExpense = i;
+            
+        }
+        //isleidau maziausiai
+        if (item.expense && item.expense > maxExpenseValue ) {
+            maxExpenseValue = item.expense;
+            maxExpense = i;
+        }
+
         /*menesiuPavadinimai[item.month-1] -1, nes sarsae yra 1-12sk, JS skaiciuoja 0-11, todel paskutis menuo - undefined*/ 
         HTML += ` <div class="table-row">
                     <div class="cell">${i+1}</div>
@@ -59,13 +89,21 @@ function lentelesGeneravimas(menesiuPavadinimai, pinigine) {
     const incomeDOM = document.querySelector('.table-footer > .cell:nth-of-type(3)');
     const expenseDOM = document.querySelector('.table-footer > .cell:nth-of-type(4)');
     const balanceDOM = document.querySelector('.table-footer > .cell:nth-of-type(5)');
+    const minIncomeDOM = document.querySelector('#minIncome');
+    const maxIncomeDOM = document.querySelector('#maxIncome');
+    const minExpenseDOM = document.querySelector('#minExpense');
+    const maxExpenseDOM = document.querySelector('#maxExpense');
     DOM.innerHTML = HTML;
-
+    
+ 
     balanceDOM.innerText = formatMoney(balance);
     incomeDOM.innerText = formatMoney(income);
     expenseDOM.innerText = formatMoney(expense);
     
-    // console.log(HTML);
+    minIncomeDOM.innerText = menesiuPavadinimai[minIncome]
+    maxIncomeDOM.innerText = menesiuPavadinimai[maxIncome]
+    minExpenseDOM.innerText = menesiuPavadinimai[minExpense]
+    maxExpenseDOM.innerText = menesiuPavadinimai[maxExpense]
 }
 
 lentelesGeneravimas(menesiai, account);
